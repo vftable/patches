17dbd1bf28876b2220474b22597d9f77677a17e5
vftable <59777379+vftable@users.noreply.github.com>
2025-01-02 13:12:34 +0000

    Update YoutubeTV.js

--- YoutubeTV.js
+++ YoutubeTV.js
@@ -1,1 +1,30 @@
+const orig = JSON.parse;
+JSON.parse = function () {
+  const r = orig.apply(this, arguments);
+
+  if (r.adPlacements) {
+    r.adPlacements = [];
+  }
+
+  if (r.playerAds) {
+    r.playerAds = false;
+  }
+
+  if (r.adSlots) {
+    r.adSlots = [];
+  }
+
+  if (r.contents
+    && r.contents.tvBrowseRenderer
+    && r.contents.tvBrowseRenderer.content
+    && r.contents.tvBrowseRenderer.content.tvSurfaceContentRenderer
+    && r.contents.tvBrowseRenderer.content.tvSurfaceContentRenderer.content
+    && r.contents.tvBrowseRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer
+    && r.contents.tvBrowseRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents
+  ) {
+    r.contents.tvBrowseRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents = r.contents.tvBrowseRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents.filter(e => !e.adSlotRenderer);
+  }
+
+  return r;
+};
 
